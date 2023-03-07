0b5e2e507dfd1c3f9e8fecff845afde60ce4c905
Yan-XYR <amandax2013.ax@gmail.com>
2023-03-07 03:32:48 -0800

    CourseCommandLineRunner.java

---
+++ Week3/JPA and Hibernate/CourseCommandLineRunner.java
@@ -0,0 +1,45 @@
+package com.yaxu.springboot.learnjpaandhibernate.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.yaxu.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.yaxu.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
+
+@Component
+public class CourseCommandLineRunner implements CommandLineRunner{
+
+//	@Autowired
+//	private CourseJdbcRepository repository;
+
+//	@Autowired
+//	private CourseJpaRepository repository;
+
+	@Autowired
+	private CourseSpringDataJpaRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new Course(1, "Learn AWS Jpa!", "1"));
+		repository.save(new Course(2, "Learn Azure Jpa!", "2"));
+		repository.save(new Course(3, "Learn DevOps Jpa!", "3"));
+		
+		repository.deleteById(1l);
+		
+		System.out.println(repository.findById(2l));
+		System.out.println(repository.findById(3l));
+		
+		System.out.println(repository.findAll());
+		System.out.println(repository.count());
+		
+		System.out.println(repository.findByAuthor("4"));
+		System.out.println(repository.findByAuthor(""));
+
+		System.out.println(repository.findByName("Learn AWS Jpa!"));
+		System.out.println(repository.findByName("Learn DevOps Jpa!"));
+
+		
+	}
+
+}
